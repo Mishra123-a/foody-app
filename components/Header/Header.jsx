dd6517473b5988b3f0bd8c9374d3a9b2a3658cdf
Satyam Mishra <108019453+Mishra123-a@users.noreply.github.com>
2024-04-17 12:12:47 +0530

    components files uploaded

---
+++ components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+    <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Indulge in a culinary journey at our vibrant restaurant. Discover exquisite flavors, impeccable service, and an inviting ambiance. A gastronomic delight awaits!  </p>
+        <button>Veiw Menu</button>
+    </div>
+      
+    </div>
+  )
+}
+
+export default Header
